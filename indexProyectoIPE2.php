2774e865e649d096dd5110881d1363e3b7c5917f
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-19 00:24:47 +0100

    fix: Eliminar iconos

--- indexProyectoIPE2.php
+++ indexProyectoIPE2.php
@@ -43,14 +43,10 @@
     </main>
     <footer id="pie">
         <div>
-            <a href="/feed/rss.xml" type="application/rss+xml" target="_blank"><i class="fa-solid fa-rss"></i></a>
             <a href="https://github.com/GonJunLor/GJLIPE2ProyectoIPE2.git" target="_blank">
                 <i class="fa-brands fa-github"></i>
             </a>
             <a href="https://mogutable.com/" target="_blank"><i class="fa-solid fa-house"></i></a>
-            <a id="w3c" href="https://validator.w3.org/check?uri=https://daw108.ieslossauces.es/108EDDProyectoEDD/index.html" target="_blank">          
-                <img src="/webroot/media/images/W3C.png" alt="Validado por W3C" width="55" >        
-            </a>
         </div>
         2024-25 IES LOS SAUCES. &#169;Todos los derechos reservados. 
         <div>
