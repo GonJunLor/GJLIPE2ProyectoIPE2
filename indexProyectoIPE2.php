12653d466e0c900b544acf9ef909b3675d28e0ca
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-18 23:18:54 +0100

    feat: :sparkles: Inicio

---
+++ indexProyectoIPE2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--
+    Autor: Gonzalo Junquera Lorenzo
+    Fecha modificación: 06/10/2025
+    Descripción: Indice general de la asignatura
+-->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Indice general de la asignatura">
+    <meta name="author" content="Gonzalo Junquera Lorenzo">
+    <meta name="keywords" content="indice, daw1, edd">
+    <meta name="generator" content="visual studio code">
+    <link rel="icon" type="image/png" href="webroot/media/favicon/favicon-32x32.png">
+    <link rel="stylesheet" href="webroot/css/fonts.css">
+    <link rel="stylesheet" href="webroot/css/estilosHome.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" type="text/css" rel="stylesheet">
+    <link rel="alternate" href="rss.xml" type="application/rss+xml" title="RSS">
+    <title>Gonzalo Junquera Lorenzo</title>
+</head>
+<body>
+    <div id="aviso">CURSO 2025/2026 -- DAW 2 -- I.E.S. LOS SAUCES</div>
+    <nav>
+        <div><a href="/">Menu</a></div>
+        <h2>IPE2 Itinerario par la empleabilidad 2</h2>
+        <h2>Gonzalo Junquera Lorenzo</h2>
+    </nav>
+    <main>
+        <section>
+            <div>
+                <article class="practica">
+                    <iframe src="manual.html"></iframe>
+                    <a href="manual.html" target="_blank">
+                        <img src="webroot/media/images/ies.png" alt="Banner">
+                        <div class="superpuesto"></div>
+                        <h4>Manual Como Arreglar grieta</h4>
+                    </a>
+                </article>
+            </div>
+        </section>
+
+    </main>
+    <footer id="pieEDD">
+        <div>
+            <a href="/feed/rss.xml" type="application/rss+xml" target="_blank"><i class="fa-solid fa-rss"></i></a>
+            <a href="https://github.com/GonJunLor/108EDDProyectoEDD" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://mogutable.com/" target="_blank"><i class="fa-solid fa-house"></i></a>
+            <a id="w3c" href="https://validator.w3.org/check?uri=https://daw108.ieslossauces.es/108EDDProyectoEDD/index.html" target="_blank">          
+                <img src="/webroot/media/images/W3C.png" alt="Validado por W3C" width="55" >        
+            </a>
+        </div>
+        2024-25 IES LOS SAUCES. &#169;Todos los derechos reservados. 
+        <div>
+            <a href="/index.html">
+            <address style="display: inline;">Gonzalo Junquera Lorenzo</address>
+            </a>
+            <time datetime="2025-10-06">06-10-2025</time>
+        </div>
+    </footer>
+</body>
+</html>
